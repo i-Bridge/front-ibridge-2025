5cb3e0be89fb9558d541bb62cdb7542b4d1956f7
윤아 남 <yoona21978@gmail.com>
2025-02-13 19:23:16 +0900

    Feat: 월 선택 기능 추가 및 api 연동 초기 단계

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -3,3 +3,5 @@ import apiClient from './index';
 
 export const postTodos = () => apiClient.post('/start/signin');
 export const createTodo = () => apiClient.post('/todos');
+
+export const getDayInfo = () => apiClient.get('/parent/{childId}/home'); //url 수정 필요
