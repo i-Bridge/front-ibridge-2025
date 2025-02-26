77c17a30ec0a52204eec288707a4c6b08aed5c28
윤아 남 <yoona21978@gmail.com>
2025-02-26 14:41:55 +0900

    Feat: 구글 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,17 @@
 import './globals.css';
 import { SessionProvider } from 'next-auth/react';
 
+import { Noto_Sans_KR } from 'next/font/google';
+
+const notoSansKR = Noto_Sans_KR({
+  weight: ['100', '300', '500', '700', '900'], // 사용할 폰트 굵기 설정
+  display: 'swap', // 폰트 로딩 방식 설정
+});
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body>
+      <body className={notoSansKR.className}>
         <SessionProvider>{children}</SessionProvider>
       </body>
     </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,13 +12,16 @@ export default {
         'i-orange': '#FF662D',
         'i-skyblue': '#57B5CE',
         'i-yellow': '#FDD504',
-        'i-ivory': '#FFF6ef', //배경
+        'i-ivory': '#FFF6ef',
         'i-lightgreen': '#C3E93F',
         'i-lightpurple': '#F1D1EC',
         'i-pink': '#FFB1D0',
-        'i-darkblue': '#034892', //기본
+        'i-darkblue': '#034892',
         'i-lightgrey': '#D9D9D9',
         'i-black': '#202123',
+      },
+      fontFamily: {
+        'noto-sans-kr': ['"Noto Sans KR"', 'serif'], // Noto Sans KR 폰트를 기본 폰트로 설정
       },
     },
   },
