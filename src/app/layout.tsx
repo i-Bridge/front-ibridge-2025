f1372dde6af871f6263e8adca3c06852763bf5c7
윤아 남 <yoona21978@gmail.com>
2025-03-18 16:44:08 +0900

    Fix: 페이지 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,15 @@
 import './globals.css';
 import { SessionProvider } from 'next-auth/react';
 
+import ParentHeader from "@/components/Header/ParentHeader"; 
 
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <body>
-        <SessionProvider>{children}</SessionProvider>
+        
+        <SessionProvider><ParentHeader/>{children}</SessionProvider>
       </body>
     </html>
   );
