fcc0535f4ec4709bc4104ba87aecda968b1e4e7e
윤아 남 <yoona21978@gmail.com>
2025-04-06 00:30:19 +0900

    Feat: profile 페이지 api 연결 코드 작성 ( 서버 연결 오류 있음 )

--- src/app/(start)/profile/page.tsx
+++ src/app/(start)/profile/page.tsx
@@ -1,31 +1,29 @@
 import Link from 'next/link';
-import { ApiResponse } from '@/types';
-import { Child } from '@/types';
 import { fetchLoginData } from '@/app/services/profile/fetchLoginData';
 
 export default async function Profile() {
-  const res: ApiResponse<
-  {
-    isSuccess: boolean;
-    isAccepted: boolean;
-    familyName: string;
-    children: Child[];
-  }, 
-  { isSuccess: boolean }
-> = await fetchLoginData();
+  const profileData = await fetchLoginData();
 
-  if (!res.data.isSuccess) {
-    return <p> 데이터 로드 실패 </p>;
+  if (!profileData) {
+    return <div>데이터를 불러오지 못했습니다.</div>; //return을 해줘야 null이 아닌 걸로 타입스크립트가 안심 가능
   }
+  if (!profileData.isAccepted) {
+    return <div> isAccepted: false </div>; 
+    }
 
-  const ProfileData = res.data;
+    if (!profileData.isSend) {
+      console.log('isSend 거짓:',profileData.isSend);
+      return <div> isSend: false </div>; 
+      }
+
+
   return (
     <div className="w-full h-screen flex flex-col bg-gray-100">
       {/* 상단: 관리 계정 프로필 */}
       <div className="flex justify-center items-center flex-wrap gap-8 p-8 bg-white border-b">
-      <p><strong>가족명:</strong> {ProfileData.familyName}</p>
+      <p><strong>가족명:</strong> {profileData.familyName}</p>
 
-        {ProfileData.children.map((child) => (
+        {profileData.children.map((child) => (
           <div key={child.id} className="flex flex-col items-center">
             <Link href={`/parent/${child.id}/home`}>
               <div className="w-32 h-32 bg-blue-500 rounded-full hover:ring-4 hover:ring-blue-300 cursor-pointer flex items-center justify-center text-white">
@@ -38,7 +36,7 @@ export default async function Profile() {
 
       {/* 하단: 자식 계정 프로필 */}
       <div className="flex justify-center items-center flex-wrap gap-8 p-8">
-      {ProfileData.children.map((child) => (
+      {profileData.children.map((child) => (
           <div key={child.id} className="flex flex-col items-center">
             <Link href={`/parent/${child.id}/home`}>
               <div className="w-32 h-32 bg-blue-500 rounded-full hover:ring-4 hover:ring-blue-300 cursor-pointer flex items-center justify-center text-white">
