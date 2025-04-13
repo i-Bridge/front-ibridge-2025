fad7ee0496e68f86474c89b091224ef917ce71e3
윤아 남 <yoona21978@gmail.com>
2025-04-13 15:06:53 +0900

    Chore: 기타 수정 및 레이아웃에서 헤더 뺌

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,14 @@
-'use client';
 
+'use client'
 import './globals.css';
 import { SessionProvider } from 'next-auth/react';
-import ParentHeader from '@/components/Header/ParentHeader';
-import { HeaderContextProvider } from '@/context/HeaderContext';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <body>
         <SessionProvider>
-          <HeaderContextProvider>
-            <ParentHeader />
             {children}
-          </HeaderContextProvider>
         </SessionProvider>
       </body>
     </html>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,5 @@
 
 export * from './analysis.types';
-export * from './api.types';
 export * from './child.types';
 export * from './question.types';
 export * from './subject.types';
