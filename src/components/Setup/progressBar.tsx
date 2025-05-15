4da3b5655dec33a5368b61f3c26ef3f657d99cdf
윤아 남 <yoona21978@gmail.com>
2025-05-15 17:04:25 +0900

    Fix: /setup 자식 1명 되고, 중복체크 메세지 수정, 디자인 수정

--- src/components/Setup/progressBar.tsx
+++ src/components/Setup/progressBar.tsx
@@ -2,20 +2,44 @@
 import { useSetupStore } from '@/store/setup/setupStore';
 
 const ProgressBar = () => {
-  const { currentChildIndex, childrenCount } = useSetupStore();
+  const { currentChildIndex, childrenCount, step } = useSetupStore();
 
   // ✅ 전체 단계 수: step1(1) + step2(자식 수)
   const totalSteps = childrenCount;
-  const progress = ((currentChildIndex + 1) / totalSteps) * 100;
+  const progress = ((currentChildIndex ) / totalSteps) * 100;
 
-  return (
-    <div className="w-full bg-gray-300 h-2 rounded-full overflow-hidden">
-      <div
-        className="h-full bg-blue-500 transition-all"
-        style={{ width: `${progress}%` }}
-      ></div>
-    </div>
-  );
+  if (step === 1) {
+    return (
+      <div>
+        <h1 className="text-black text-sm text-center mb-2">
+          가족 등록
+        </h1>
+        <div className="w-full bg-gray-300 h-2 rounded-full ">
+          <div
+            className="h-full bg-green-200 transition-all"
+            style={{ width: `100%` }}
+          ></div>
+        </div>
+      </div>
+    );
+  }
+  if (step === 2) {
+    return (
+      <div>
+        
+        <h1 className="text-black text-sm text-center mb-2">
+        
+          자녀 등록({currentChildIndex }/{childrenCount})
+        </h1>
+        <div className="w-full bg-gray-300 h-2 rounded-full overflow-hidden">
+          <div
+            className="h-full bg-blue-500 transition-all"
+            style={{ width: `${progress}%` }}
+          ></div>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default ProgressBar;
--- src/components/Setup/setup.tsx
+++ src/components/Setup/setup.tsx
@@ -6,8 +6,6 @@ import Step2 from './step2';
 const Setup = () => {
   const { step, childrenCount, currentChildIndex } = useSetupStore();
 
-  console.log("현재 step:", step); // ✅ 디버깅용 로그 추가
-
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
       {step === 1 && <Step1 />}
