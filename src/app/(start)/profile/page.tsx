cc64bc953fb624b4573cbc0ef1a675e65e1d51cf
윤아 남 <yoona21978@gmail.com>
2025-05-25 14:37:47 +0900

    Design: 프로필 선택 디자인 수정

--- src/app/(start)/profile/page.tsx
+++ src/app/(start)/profile/page.tsx
@@ -29,36 +29,48 @@ export default async function Profile() {
   console.log('받아온 profileData:', profileData); // 추후 삭제 예정
 
   return (
-    <div className="w-full h-screen flex flex-col">
-      {/* 상단: 관리 계정 프로필 */}
-      <div className="flex justify-center items-center flex-wrap gap-8 p-8  bg-white ">
+    <div className="w-full h-screen flex flex-col ">
+      {/* 가족 이름 */}
+      <div className="w-full mt-8 p-8 text-4xl font-semibold text-center ">
         <p>
-          <strong>가족명:</strong> {profileData.familyName}
+          <strong>🏠 </strong> {profileData.familyName}
+          <strong> 🏠</strong>
         </p>
+      </div>
 
+      {/* 부모 계정 프로필 */}
+      <div className="flex justify-center items-center flex-wrap gap-8 p-8">
         {profileData.children.map((child) => (
           <div key={child.id} className="flex flex-col items-center ">
             <Link href={`/parent/${child.id}/home`}>
-              <div className="w-32 h-32 bg-i-skyblue rounded-full hover:ring-4 hover:ring-blue-300 cursor-pointer flex items-center justify-center text-white">
-                {child.name} 부모님
+              <div className="text-center break-words text-xl w-24 h-24  bg-i-lightgreen/70 rounded-full  hover:shadow-md hover:bg-i-lightgreen cursor-pointer flex items-center justify-center text-white">
+                {child.name}
               </div>
+              
             </Link>
+            <span className="text-center text-sm text-gray-400 mt-2">
+                부모님
+              </span>
           </div>
         ))}
       </div>
 
-      {/* 하단: 자식 계정 프로필 */}
-      <div className="flex justify-center items-center flex-wrap gap-8 p-8 ml-32">
+      {/*<ul className=" border-t "></ul>*/}
+
+      {/* 자식 계정 프로필 */}
+      <div className="flex justify-center items-center flex-wrap gap-8 p-8  bg-orange-100 ">
         {profileData.children.map((child) => (
           <div key={child.id} className="flex flex-col items-center">
             <Link href={`/child/${child.id}/home`}>
-              <div className="w-32 h-32 bg-i-lightpurple rounded-full hover:ring-4 hover:ring-blue-300 cursor-pointer flex items-center justify-center text-white">
+              <div className="text-2xl w-32 h-32 bg-i-lightorange/70 rounded-full hover:shadow-md hover:bg-i-lightorange cursor-pointer flex items-center justify-center text-white  text-center break-words">
                 {child.name}
               </div>
             </Link>
           </div>
         ))}
       </div>
+
+      <div className="w-full h-[200px] bg-orange-100"></div>
     </div>
   );
 }
