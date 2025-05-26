b508825bf6ed2b88f3f4aff50e0fa58740524ab1
윤아 남 <yoona21978@gmail.com>
2025-05-27 00:39:05 +0900

    Chore: 오타 수정

--- src/components/Setup/step2.tsx
+++ src/components/Setup/step2.tsx
@@ -161,8 +161,8 @@ const Step2 = () => {
           onClick={handlePrevious}
           className="text-gray-900  px-2 py-2"
         >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
 </svg>
 
         </button>
