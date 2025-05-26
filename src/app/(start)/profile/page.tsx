c8d7d8e325c9666df57c20e91bfb4b08d896402e
rin00000 <rinjun@naver.com>
2025-05-26 17:41:31 +0900

    Chore: 프로필 페이지에서 child 이동 경로 변경

--- src/app/(start)/profile/page.tsx
+++ src/app/(start)/profile/page.tsx
@@ -51,7 +51,7 @@ export default async function Profile() {
       <div className="flex justify-center items-center flex-wrap gap-8 p-8 ml-32">
         {profileData.children.map((child) => (
           <div key={child.id} className="flex flex-col items-center">
-            <Link href={`/child/${child.id}/home`}>
+            <Link href={`/child/${child.id}/chat`}>
               <div className="w-32 h-32 bg-i-lightpurple rounded-full hover:ring-4 hover:ring-blue-300 cursor-pointer flex items-center justify-center text-white">
                 {child.name}
               </div>
