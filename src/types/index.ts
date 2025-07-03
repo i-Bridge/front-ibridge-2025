e2ebafa5afe8ddfff5b8e3a440b561bcf55da965
rin00000 <rinjun@naver.com>
2025-07-03 15:54:51 +0900

    Fix: 페이지 params 타입 수정

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,4 @@
-
 export * from './analysis.types';
 export * from './child.types';
 export * from './question.types';
 export * from './subject.types';
-export * from './user.types';
-
---
+++ src/types/page-props.ts
@@ -0,0 +1,5 @@
+export interface ChildPageParams {
+  params: {
+    childId: string;
+  };
+}
