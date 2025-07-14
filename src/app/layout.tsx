047f7fa546378b247e369005ca763c585906d928
rin00000 <rinjun@naver.com>
2025-07-14 16:57:14 +0900

    Feat: sonner(토스트팝업) 설치 후 layout에 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
 'use client';
 import './globals.css';
 import { SessionProvider } from 'next-auth/react';
+import { Toaster } from 'sonner';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <body>
-        <SessionProvider>{children}</SessionProvider>
+        <SessionProvider>
+          {' '}
+          <Toaster position="top-center" richColors />
+          {children}
+        </SessionProvider>
       </body>
     </html>
   );
