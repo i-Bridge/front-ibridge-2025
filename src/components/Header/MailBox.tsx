51986b19c85c58caddb4e75acf238a93839073bb
윤아 남 <yoona21978@gmail.com>
2025-07-19 15:44:45 +0900

    Fix: 자잘한 수정

--- src/components/Header/MailBox.tsx
+++ src/components/Header/MailBox.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import {  useState } from 'react';
 import { Fetcher } from '@/lib/fetcher';
 import { useRouter } from 'next/navigation';
 
