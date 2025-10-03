6242804fa4b2c7948c4c7127abb04eb11a01802e
윤아 남 <yoona21978@gmail.com>
2025-10-03 13:47:34 +0900

    Design: 부모님 바로가기, 셋업 ui 세부 변경

--- src/app/(start)/profile/_components/ParentDropDown.tsx
+++ src/app/(start)/profile/_components/ParentDropDown.tsx
@@ -15,20 +15,7 @@ export default function ParentDropdown({ childrenData }: { childrenData: Child[]
     >
       {/* 아이콘 버튼 */}
       <button className="p-2 rounded-full  hover:bg-gray-100">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6 text-gray-600"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z"
-          />
-        </svg>
+        <p> 부모님 </p>
       </button>
 
 
