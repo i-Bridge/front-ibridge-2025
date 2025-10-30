3bfac3b4b41f4a080bd87fee299e94e44c569716
윤아 남 <yoona21978@gmail.com>
2025-10-31 04:45:28 +0900

    Feat: 계정 확인 중 컴포넌트

---
+++ src/app/(start)/_components/AccountCheckingForm.tsx
@@ -0,0 +1,28 @@
+import { Text } from '@/ui/Text';
+import RotatingSpinner from '@/ui/RotatingSpinner';
+import LogoutButton from '@/components/Auth/LogoutButton';
+
+interface AccountCheckingProps {
+  userName: string;
+}
+
+export default function AccountCheckingForm({ userName }: AccountCheckingProps) {
+    const displayUserName = userName || '사용자';
+
+  return (
+    <div className="flex flex-col items-center justify-center self-stretch gap-7">
+      <RotatingSpinner />
+
+      <div className="flex flex-col items-center justify-start gap-2">
+        <Text variant="title01" className=" text-grayscale-gray90 ">
+          {displayUserName} 님, 반가워요!
+        </Text>
+
+        <Text variant="body04" className="text-grayscale-gray60 ">
+          계정 확인 중입니다.
+        </Text>
+      </div>
+      <LogoutButton>로그아웃</LogoutButton>
+    </div>
+  );
+}
