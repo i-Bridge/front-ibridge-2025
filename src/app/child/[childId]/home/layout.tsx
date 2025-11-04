d88b043228e9da6dc2b1e7b2a0adb42cd6cf8de6
윤아 남 <yoona21978@gmail.com>
2025-11-04 23:16:35 +0900

    Refactor: 자녀 홈 헤더

--- src/app/child/[childId]/home/layout.tsx
+++ src/app/child/[childId]/home/layout.tsx
@@ -1,7 +1,7 @@
-import Header, {
+import ChildHeaderLayout, {
   ChildName,
   GrapeInfo,
-} from '../_components/header/ChildHeader';
+} from '@/app/child/[childId]/_components/header/ChildHeader';
 
 export default function HomeLayout({
   children,
@@ -11,8 +11,13 @@ export default function HomeLayout({
   return (
     <div>
       {/* Header.ChildName 대신 ChildName 컴포넌트를 직접 사용합니다. */}
-      <Header left={<ChildName />} right={<GrapeInfo />} />
-      <main className="pt-16">{children}</main>
+      <ChildHeaderLayout
+        className="bg-white"
+        left={<ChildName />}
+        right={<GrapeInfo />}
+      />
+
+      <main className="">{children}</main>
     </div>
   );
 }
